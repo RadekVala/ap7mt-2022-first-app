f5d4dbe24f7aa49613e71e5fc58276f60a339e17
vala <vala@utb.cz>
2022-10-13 09:26:52 +0200

    Save args enabled Second fragment random number functionality added

--- app/src/main/java/com/example/myfirstapp/SecondFragment.kt
+++ app/src/main/java/com/example/myfirstapp/SecondFragment.kt
@@ -1,12 +1,15 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.myfirstapp.databinding.FragmentSecondBinding
+import androidx.navigation.fragment.navArgs
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -20,8 +23,8 @@ class SecondFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
@@ -29,10 +32,26 @@ class SecondFragment : Fragment() {
 
     }
 
+    val args: SecondFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
+        Log.v("MYSUPERAPP", args.myArg.toString())
+
+        val count = args.myArg
+        val countText = getString(R.string.random_hint_text, count)
+        binding.randomHint.text = countText
+
+        val random = java.util.Random()
+        var randomNumber = 0
+        if (count > 0) {
+            randomNumber = random.nextInt(count + 1)
+        }
+
+        binding.tvRandomNumber.text = randomNumber.toString()
+
+        binding.btnPrev.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
